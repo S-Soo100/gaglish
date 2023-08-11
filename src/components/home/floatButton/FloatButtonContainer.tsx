3c4e49e7258eb28610ec9178f56f73ba36777c42
백승수 <baek@BaekBookPro-M1.local>
2023-08-11 14:16:07 +0900

    홈 화면 하단의 float 버튼 제작

---
+++ src/components/home/floatButton/FloatButtonContainer.tsx
@@ -0,0 +1,9 @@
+import FloatButtonView from "./FloatButtonView";
+
+export default function FloatButtonContainer() {
+  const floatButtonProp = {
+    link: "/card/add",
+  };
+
+  return <FloatButtonView {...floatButtonProp} />;
+}
